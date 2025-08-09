2b78b587d88d9f6f00e0b95446153564906b2450
Terry Child <tc@moohar.com>
2025-08-09 17:47:36 +0100

    Add basic memory management

---
+++ src/lib/mem.c
@@ -0,0 +1,10 @@
+#include "mem.h"
+#include "console.h"
+
+void* allocate(void* data, size_t size) {
+    void* new_data = realloc(data, size);
+    if (new_data == NULL) {
+        PANIC("unable to allocate memory");
+    }
+    return new_data;
+}
---
+++ src/lib/mem.h
@@ -0,0 +1,8 @@
+#ifndef LIB_MEM_H
+#define LIB_MEM_H
+
+#include <stdlib.h>
+
+void* allocate(void* data, size_t size)
+
+#endif
