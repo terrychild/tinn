7ca62033ad615a8d2d58270b82023ff90b0fb6ed
Terry Child <tc@moohar.com>
2026-09-06 12:30:09 +0100

    Add types and useful macros

--- src/lib.h
+++ src/lib.h
@@ -1,6 +1,8 @@
 #ifndef LIB_H
 #define LIB_H
 
+#include "lib/types.h"
+#include "lib/macros.h"
 #include "lib/console.h"
 #include "lib/cli.h"
 
---
+++ src/lib/macros.h
@@ -0,0 +1,13 @@
+#ifndef LIB_MACROS_H
+#define LIB_MACROS_H
+
+#include "lib/types.h"
+
+#define STRINGIZER(x) #x
+#define STR(x) STRINGIZER(x)
+
+#define KB(x) (U64) (x * 1024)
+#define MB(x) (U64) (x * 1024 * 1024)
+#define GB(x) (U64) (x * 1024 * 1024 * 1024)
+
+#endif
